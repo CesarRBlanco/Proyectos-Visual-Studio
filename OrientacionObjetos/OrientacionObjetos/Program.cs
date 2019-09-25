f4696f5aaf88efc60242ecf483240563280555f6
Cesar <white.h0und.f0x@gmail.com>
2019-09-25 12:49:00 +0200

    Fallo y recu

---
+++ OrientacionObjetos/OrientacionObjetos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacionObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
