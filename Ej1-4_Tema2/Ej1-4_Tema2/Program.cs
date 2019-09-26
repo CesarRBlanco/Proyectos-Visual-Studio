839fab2600975be547dc8cc0a669b4992851a1b8
Cesar <white.h0und.f0x@gmail.com>
2019-09-26 10:43:56 +0200

    10.45 - [26/09/19]

--- Ej1-4_Tema2/Ej1-4_Tema2/Program.cs
+++ Ej1-4_Tema2/Ej1-4_Tema2/Program.cs
@@ -10,6 +10,55 @@ namespace Ej1_4_Tema2
     {
         static void Main(string[] args)
         {
+
         }
+    }
+
+    class Persona
+    {
+        int age, dniNum;
+        String dni, name, surname, dniLet="TRWAGMYFPDXBNJZSQVHLCKE";
+
+        public int Edad
+        {
+            set
+            {
+                if (age < 0)
+                {
+                    this.age = 0;
+                }
+                else
+                {
+                    this.age = value;
+                }
+            }
+            get
+            {
+                return age;
+            }
+        }
+        public String Dni
+        {
+            set
+            {
+
+                if ((dni.Length == 8) && (Int32.TryParse(value, out this.dniNum)))
+                {
+                    this.dni = value;
+                }
+                else
+                {
+                    Console.WriteLine("DNI is invalid. Please, try again.");
+                }
+            }
+            get
+            {
+                int dniCal = dniNum % 23;
+                dni = (dniNum.ToString() + dniLet.ElementAt(dniCal));
+                    return dni;
+            }
+        }
+        public String Name { set; get; }
+        public String Surname { set; get; }
     }
 }
