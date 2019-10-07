4507ef596e908c6babf67b3ebf68cf5fd82b84fa
Cesar (Clase) <white.h0und.f0x@gmail.com>
2019-10-07 12:34:26 +0200

    12:30 - [07/10/19]

--- Tema3_Ej2/Tema3_Ej2/Tema3_Ej2/Program.cs
+++ Tema3_Ej2/Tema3_Ej2/Tema3_Ej2/Program.cs
@@ -10,6 +10,34 @@ namespace Tema3_Ej2
     {
         static void Main(string[] args)
         {
+            int[,] tableNotes = new int[4, 12];
+            int note;
+            Random notas = new Random();
+
+            for (int i = 0; i < tableNotes.GetLength(0); i++)
+            {
+                for (int j = 0; j < tableNotes.GetLength(1); j++)
+                {
+                    note = notas.Next(1, 101);
+                    switch (note)
+                    {
+                        case int n when (n <= 15):
+                            note= 2;
+                        case int n when ((n > 15) && (n <= 40)):
+                            return 'x';
+                        case int n when (n > 40):
+                            return '1';
+                    }
+                    return '0';
+                }
+                tableNotes[i, j] = note;
+
+            }
         }
+
+
+
     }
 }
+}
+
