a2dbf839fa80eef29a7b5f9b860afa4e3799f861
Cesar (Clase) <white.h0und.f0x@gmail.com>
2019-10-14 17:35:46 +0200

    18:30 - [14/10/19]

---
+++ Servicios/Tema 1/Tema4_Ej1/Tema4_Ej1/Tema4_Ej1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema4_Ej1
+{
+    class Program
+    {
+        static void f1()
+        {
+            Console.WriteLine("A");
+        }
+
+        static void f2()
+        {
+            Console.WriteLine("B");
+        }
+
+        static void f3()
+        {
+            Console.WriteLine("C");
+        }
+
+        public static void MenuGenerator(string[] vector, Delegate[] delegates)
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            //MenuGenerator(new string[] { "Op1", "Op2", "Op3" },
+            //    new MyDelegate[] { f1, f2, f3 });
+        }
+    }
+}
