bd88f580f61305dbe9f9f1b38fe4d79ed543d336
Cesar (Clase) <white.h0und.f0x@gmail.com>
2019-10-21 12:25:43 +0200

    12:15 - [21/10/19]

--- Tema 3/Tema3_Ej3/T3_Ejercicio3/T3_Ejercicio3/Program.cs
+++ Tema 3/Tema3_Ej3/T3_Ejercicio3/T3_Ejercicio3/Program.cs
@@ -6,10 +6,71 @@ using System.Threading.Tasks;
 
 namespace T3_Ejercicio3
 {
+    public delegate void MyDelegate();
+
+    class Videogames
+    {
+        string title;
+        int year;
+
+        enum eGenre
+        {
+            Arcade,
+            Videoaventura,
+            Shootemup,
+            Estrategia,
+            Deportivo
+        }
+    }
+
+    class Menu
+    {
+        MyDelegate[] delegates =
+        {
+        };
+
+        public void menu(string[] options)
+        {
+            for (int i = 0; i < options.Length; i++)
+            {
+                Console.WriteLine("{0}. {1}.", i + 1, options[i]);
+            }
+            Console.WriteLine("0. Exit.");
+
+        }
+
+        public void insertGames()
+        {
+        }
+
+        public void deleteGames()
+        {
+        }
+
+        public void visualizeGames()
+        {
+        }
+
+        public void visualizeOneGenre()
+        {
+        }
+
+        public void modifyGames()
+        {
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            string[] options =
+            {
+                "Insert a new game", "Delete games", "Visualize all games", "Visualize one genre", "Modify a game"
+            };
+            Menu mn = new Menu();
+            mn.menu(options);
+            Console.ReadKey();
         }
     }
 }
