525786f84f773f67b48ae10f0c6ac4095f0d5bfe
Cesar (Clase) <white.h0und.f0x@gmail.com>
2019-10-24 13:59:25 +0200

    [005]

--- Desarrollo de Interfaces/Tema 3/Tema3_Ej3/T3_Ejercicio3/T3_Ejercicio3/Program.cs
+++ Desarrollo de Interfaces/Tema 3/Tema3_Ej3/T3_Ejercicio3/T3_Ejercicio3/Program.cs
@@ -1,17 +1,16 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+
 
 namespace T3_Ejercicio3
 {
-    public delegate void MyDelegate();
+    public delegate void MyDelegate(String titleGame, int yearGame, int genreIndex);
 
-    class Videogames
+    class Videogames : IComparable<Videogames>
     {
+        public int GenreIndex { set; get; }
+
         public string Title { set; get; }
 
         public int Year { set; get; }
@@ -20,43 +19,55 @@ namespace T3_Ejercicio3
         {
             Arcade,
             Aventuras,
-            Shooter,
             Estrategia,
-            Pelea
+            Pelea,
+            Shooter
         }
 
+        eGenre[] genresArray = {eGenre.Arcade, eGenre.Aventuras, eGenre.Estrategia, eGenre.Pelea, eGenre.Shooter};
 
-        public Videogames(String titleGame, int yearGame)
+        public Videogames(String titleGame, int yearGame, int genreIndex)
         {
             Title = titleGame;
             Year = yearGame;
-        }
-
-        public Videogames()
-        {
-            Title = "";
-            Year = 0;
+            GenreIndex = genreIndex;
         }
 
         public override string ToString()
         {
-            string datos = ""+Title+" "+Year;
+            string datos = "" + Title + " // " + Year + " // " + genresArray[GenreIndex];
             return datos;
+        }
+
+
+        public int CompareTo(Videogames other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+
+            var yearGameComparison = Year.CompareTo(other.Year);
+            if (yearGameComparison != 0) return yearGameComparison;
+
+            var titleComparison = string.Compare(Title, other.Title, StringComparison.Ordinal);
+            if (titleComparison != 0) return titleComparison;
+
+            return Year.CompareTo(other.Year);
         }
     }
 
-    class VGManagement
+    class VideoGamesHandler
     {
-        List<Object> GameLibrary = new List<object>();
+        static List<Videogames> GameLibrary = new List<Videogames>();
 
-        public void adder(String titleGame, int yearGame)
+        public static void addVideoGame(String titleGame, int yearGame, int genreIndex)
         {
-            Object game1 = new Videogames(titleGame, yearGame);
-            GameLibrary.Add(game1);
+            Videogames newGame = new Videogames(titleGame, yearGame, genreIndex);
+            GameLibrary.Add(newGame);
+            GameLibrary.Sort();
         }
 
-        public void shower()
-        { 
+        public static void showVideoGames(String titleGame, int yearGame, int genreIndex)
+        {
             foreach (Object obj in GameLibrary)
             {
                 Console.WriteLine(obj);
@@ -64,19 +75,18 @@ namespace T3_Ejercicio3
         }
     }
 
-    class Menu
+    class Menu : VideoGamesHandler
     {
-        MyDelegate[] delegates =
-        {
-        };
+        static MyDelegate[] delegates = {addVideoGame, showVideoGames};
 
 
-        public void menu()
+        public static void menu()
         {
-            int opt;
+            String titleGame = "";
+            int yearGame = 0, genreIndex = 0, opt;
             string[] options =
             {
-                "Insert a new game", "Delete games", "Visualize all games", "Visualize one genre", "Modify a game"
+                "Insert a new game", "Visualize all games", "Delete games", "Visualize one genre", "Modify a game"
             };
 
             do
@@ -88,7 +98,9 @@ namespace T3_Ejercicio3
 
                 Console.WriteLine("0. Exit.");
                 opt = Int32.Parse(Console.ReadLine());
-
+                titleGame = Console.ReadLine();
+                yearGame = Int32.TryParse(Console.ReadLine());
+                genreIndex = Int32.TryParse(Console.ReadLine());
                 if (opt <= 0 || opt >= options.Length + 1)
                 {
                     Console.WriteLine("---------");
@@ -99,7 +111,7 @@ namespace T3_Ejercicio3
                 else
                 {
                     Console.WriteLine("---------");
-                    delegates[opt - 1]();
+                    delegates[opt - 1](titleGame, yearGame, genreIndex);
                     Console.WriteLine("---------");
                 }
             } while (opt != 0);
@@ -111,16 +123,21 @@ namespace T3_Ejercicio3
     {
         static void Main(string[] args)
         {
-            String[] defaultGamesTitles = {"Monster Hunter World: Iceborne", "Apex Legends", "Horizon Zero Dawn"};
-            int[] defaultGamesYears = { 2019, 2018, 2017 };
-            int[] defaultGamesGenres = { 1,2,1};
-            VGManagement defaultGames = new VGManagement();
+            String[] defaultGamesTitles = {"Monster Hunter World: Iceborne", "Horizon Zero Dawn", "Apex Legends"};
+            int[] defaultGamesYears = {2019, 2017, 2018};
+            int[] defaultGamesGenres = {1, 1, 4};
+
+            //----------------------------------------------------------------------------------------------------------
             for (int i = 0; i < defaultGamesTitles.Length; i++)
             {
-                defaultGames.adder(defaultGamesTitles[i],defaultGamesYears[i]);
+                VideoGamesHandler.addVideoGame(defaultGamesTitles[i], defaultGamesYears[i], defaultGamesGenres[i]);
             }
-            defaultGames.shower();
-            Console.ReadKey();
+
+            Menu.menu();
+
+
+            //defaultGames.showVideoGames();
+            //Console.ReadKey();
         }
     }
 }
