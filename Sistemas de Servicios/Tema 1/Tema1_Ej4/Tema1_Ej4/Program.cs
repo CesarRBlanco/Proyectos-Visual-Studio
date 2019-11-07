80e76c3803021e777206b55a0a000e6bf56ae9b7
Cesar <white.h0und.f0x@gmail.com>
2019-11-07 10:57:21 +0100

    [011 A]

--- Sistemas de Servicios/Tema 1/Tema1_Ej4/Tema1_Ej4/Program.cs
+++ Sistemas de Servicios/Tema 1/Tema1_Ej4/Tema1_Ej4/Program.cs
@@ -7,67 +7,36 @@ namespace Tema1_Ej4
     {
         static object l = new object();
 
-        public static void horseRun(int horseNumber)
-        {
-            int correr;
-            Random run = new Random();
-
-            for (int j = 0; j < 50; j++)
-            {
-                correr = run.Next(0, 5);
-
-                lock (l)
-                {
-                    Console.SetCursorPosition(j, horseNumber);
-
-                    Console.WriteLine("/");
-
-                    Thread.Sleep(50);
-
-                }
-
-                if (j==49)
-                {
-
-                }
-                //Thread.Sleep(100);
-            }
-        }
-
         public static void setRace()
         {
-            for (int i2 = 0; i2 < 4; i2++)
+            for (int i = 0; i < 4; i++)
             {
-                for (int i = 0; i < 50; i++)
+                for (int j = 0; j < 50; j++)
                 {
-                    Console.SetCursorPosition(i, i2);
-                    Console.Write("*");
+                    Console.SetCursorPosition(j, i);
+                    Console.Write("-");
                 }
+            }
+
+            for (int x=0;x<4;x++)
+            {
+                Console.SetCursorPosition(50,x);
+
             }
         }
 
+        public static void horseOne()
+        {
+
+        }
 
         static void Main(string[] args)
         {
             setRace();
             Console.ReadKey();
-            Thread horse1 = new Thread(() => horseRun(0));
+            Thread horse1 = new Thread(horseOne);
             horse1.Start();
-            Thread horse2 = new Thread(() => horseRun(1));
-            horse2.Start();
             Console.ReadKey();
-            horse1.Abort();
-            if (horse1.IsAlive == false)
-            {
-                Console.WriteLine("acabo 1");
-            }
-
-            if (horse2.IsAlive == false)
-            {
-                Console.WriteLine("acabo 2");
-            }
-
-
             Console.ReadKey();
         }
     }
