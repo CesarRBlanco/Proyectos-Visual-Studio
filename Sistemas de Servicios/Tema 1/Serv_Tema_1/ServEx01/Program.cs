31a495ca1ee3ed42883eab6012008a723d659ed6
Cesar <white.h0und.f0x@gmail.com>
2020-01-20 12:17:11 +0100

    [035 A]

--- Sistemas de Servicios/Tema 1/Serv_Tema_1/ServEx01/Program.cs
+++ Sistemas de Servicios/Tema 1/Serv_Tema_1/ServEx01/Program.cs
@@ -8,8 +8,59 @@ namespace ServEx01
 {
     class Program
     {
+        public delegate void MyDelegate();
+        public static void MenuGenerator(string[] options, MyDelegate[] functions)
+        {
+            int selecOpt = 0;
+            MyDelegate selecFunct;
+            do
+            {
+                try
+                {
+                    for (int i = 0; i < options.Length; i++)
+                    {
+                        Console.WriteLine("{0}. {1}.", i, options[i]);
+                    }
+                    Console.WriteLine("3. Exit.");
+                    selecOpt = Int32.Parse(Console.ReadLine());
+                    if (selecOpt == 3)
+                    {
+                        return;
+                    }
+                    selecFunct = new MyDelegate(functions[selecOpt]);
+                    selecFunct();
+                }
+                catch (SystemException e)
+                {
+                    if (e is IndexOutOfRangeException || e is OverflowException)
+                    {
+                        Console.WriteLine("The selected number is out of bounds.");
+                    }
+                    if (e is FormatException)
+                    {
+                        Console.WriteLine("A numeric value is needed.");
+
+                    }
+                }
+            } while (selecOpt != 3);
+        }
+        static void f1()
+        {
+            Console.WriteLine("A");
+        }
+        static void f2()
+        {
+            Console.WriteLine("B");
+        }
+        static void f3()
+        {
+            Console.WriteLine("C");
+        }
         static void Main(string[] args)
         {
-        }
+            MenuGenerator(new string[] { "Op1", "Op2", "Op3" },
+                          new MyDelegate[] { f1, f2, f3 });
+            Console.ReadKey();
+        }
     }
 }
---
+++ Sistemas de Servicios/Tema 1/Serv_Tema_1/ServEx02/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServEx02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+           
+
+            Process[] processes = Process.GetProcesses();
+
+            //@TODO tamaño fijo para el PID y el Name para que no descuadre nada al mostrar en el textbox. Nombre de ventana.
+
+
+            const string FORMAT = "{0,7} || {1,20}";
+                string Info="";
+            Console.WriteLine(FORMAT, "PID", "Name");
+                foreach (Process p in processes)
+                {
+                    Info = Info + String.Format(FORMAT, p.Id, p.ProcessName)+"\r\n";
+                }
+            textBox1.Text = Info;
+        }
+    }
+}
