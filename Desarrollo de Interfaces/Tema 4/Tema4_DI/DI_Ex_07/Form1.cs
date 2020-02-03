0edb0c364e4c02cbe80c32a8a59d10a4b294815e
Cesar <white.h0und.f0x@gmail.com>
2020-02-03 17:35:48 +0100

    [045.5 A]

--- Desarrollo de Interfaces/Tema 4/Tema4_DI/DI_Ex_07/Form1.cs
+++ Desarrollo de Interfaces/Tema 4/Tema4_DI/DI_Ex_07/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,67 @@ namespace DI_Ex_07
         private void AjusteDeLineaToolStripMenuItem_Click(object sender, EventArgs e)
         {
             //ajusteDeLineaToolStripMenuItem.Checked = false;
+        }
+
+        private void NuevoDocumentoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dr;
+            if (txtbox.Text != "")
+            {
+
+                dr = MessageBox.Show("Deseas guardar antes de abrir un nuevo archivo?", "Atención", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                if (dr == DialogResult.OK)
+                {
+                    if (GuardarToolStripMenuItem_Click(sender, e) == true)
+                    {
+                        txtbox.Text = "";
+                    }
+
+                }
+                else if (dr == DialogResult.No)
+                {
+                    txtbox.Text = "";
+                }
+                else
+                {
+
+                }
+
+            }
+            else
+            {
+                txtbox.Text = "";
+            }
+        }
+
+        private bool GuardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult drS;
+
+                SaveFileDialog saveDialog = new SaveFileDialog();
+
+                saveDialog.Title = "Selección de directorio para almacenar datos";
+                saveDialog.InitialDirectory = "C:\\";
+                saveDialog.Filter = "texto (*.txt)|*.txt|pascal (*.pas) |*.pas|visualbasic(*.vb) | *.vb | Todos los archivos| *.* ";
+                saveDialog.ValidateNames = true;
+                drS = saveDialog.ShowDialog();
+                if (drS == DialogResult.OK)
+                {
+                    return true;
+                }
+                StreamWriter s;
+                s = new StreamWriter(saveDialog.FileName);
+                s.Write(txtbox.Text);
+                s.Close();
+            }
+            catch (System.ArgumentException)
+            {
+
+
+            }
+            return false;
         }
     }
 }
