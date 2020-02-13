93244d6f4725b451959853ff6c4980324a1a7a8c
Cesar <white.h0und.f0x@gmail.com>
2020-02-13 10:35:43 +0100

    [051 A] :)

--- Sistemas de Servicios/Tema 2/Serv_Tema_2/ServEx02/Form1.Designer.cs
+++ Sistemas de Servicios/Tema 2/Serv_Tema_2/ServEx02/Form1.Designer.cs
@@ -28,25 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this._txtbox = new System.Windows.Forms.TextBox();
+            this._btnNew = new System.Windows.Forms.Button();
+            this._btnSave = new System.Windows.Forms.Button();
+            this._btnOpen = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // _txtbox
             // 
-            this.textBox1.Location = new System.Drawing.Point(-4, 0);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(804, 426);
-            this.textBox1.TabIndex = 0;
+            this._txtbox.Location = new System.Drawing.Point(12, 0);
+            this._txtbox.Multiline = true;
+            this._txtbox.Name = "_txtbox";
+            this._txtbox.Size = new System.Drawing.Size(776, 409);
+            this._txtbox.TabIndex = 0;
+            this._txtbox.KeyDown += new System.Windows.Forms.KeyEventHandler(this._txtbox_KeyDown);
+            // 
+            // _btnNew
+            // 
+            this._btnNew.Location = new System.Drawing.Point(12, 415);
+            this._btnNew.Name = "_btnNew";
+            this._btnNew.Size = new System.Drawing.Size(75, 23);
+            this._btnNew.TabIndex = 1;
+            this._btnNew.Text = "New";
+            this._btnNew.UseVisualStyleBackColor = true;
+            this._btnNew.Click += new System.EventHandler(this._btnNew_Click);
+            // 
+            // _btnSave
+            // 
+            this._btnSave.Location = new System.Drawing.Point(93, 416);
+            this._btnSave.Name = "_btnSave";
+            this._btnSave.Size = new System.Drawing.Size(75, 23);
+            this._btnSave.TabIndex = 2;
+            this._btnSave.Text = "Save";
+            this._btnSave.UseVisualStyleBackColor = true;
+            this._btnSave.Click += new System.EventHandler(this._btnSave_Click);
+            // 
+            // _btnOpen
+            // 
+            this._btnOpen.Location = new System.Drawing.Point(176, 416);
+            this._btnOpen.Name = "_btnOpen";
+            this._btnOpen.Size = new System.Drawing.Size(75, 23);
+            this._btnOpen.TabIndex = 3;
+            this._btnOpen.Text = "Open";
+            this._btnOpen.UseVisualStyleBackColor = true;
+            this._btnOpen.Click += new System.EventHandler(this._btnOpen_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this._btnOpen);
+            this.Controls.Add(this._btnSave);
+            this.Controls.Add(this._btnNew);
+            this.Controls.Add(this._txtbox);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -54,7 +93,10 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox _txtbox;
+        private System.Windows.Forms.Button _btnNew;
+        private System.Windows.Forms.Button _btnSave;
+        private System.Windows.Forms.Button _btnOpen;
     }
 }
 
--- Sistemas de Servicios/Tema 2/Serv_Tema_2/ServEx02/Form1.cs
+++ Sistemas de Servicios/Tema 2/Serv_Tema_2/ServEx02/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,159 @@ namespace ServEx02
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void _btnNew_Click(object sender, EventArgs e)
+        {
+            DialogResult dr;
+            if (_txtbox.Text != "")
+            {
+                dr = MessageBox.Show("Would you like to save before opening another file?", "Warning", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                if (dr == DialogResult.Yes)
+                {
+                    if (saveFile() == true)
+                    {
+                        _txtbox.Text = "";
+                    }
+                }
+                else if (dr == DialogResult.No)
+                {
+                    _txtbox.Text = "";
+                }
+            }
+            else
+            {
+                _txtbox.Text = "";
+            }
+        }
+
+
+        private bool saveFile()
+        {
+            try
+            {
+                DialogResult drS;
+                SaveFileDialog saveDialog = new SaveFileDialog();
+
+                saveDialog.Title = "Directory selection for saving";
+                saveDialog.InitialDirectory = "C:\\";
+                saveDialog.Filter = "Text files (*.txt)|*.txt| All files| *.* ";
+                saveDialog.ValidateNames = true;
+                drS = saveDialog.ShowDialog();
+                if (drS == DialogResult.OK)
+                {
+                    StreamWriter s;
+                    s = new StreamWriter(saveDialog.FileName);
+                    s.Write(_txtbox.Text);
+                    s.Close();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (System.ArgumentException)
+            {
+            }
+            return false;
+        }
+
+        private void _btnSave_Click(object sender, EventArgs e)
+        {
+            saveFile();
+        }
+
+        private void _btnOpen_Click(object sender, EventArgs e)
+        {
+
+            DialogResult dr;
+            if (_txtbox.Text != "")
+            {
+                dr = MessageBox.Show("Would you like to save before opening another file?", "Warning", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                if (dr == DialogResult.Yes)
+                {
+                    if (saveFile() == true)
+                    {
+                        openFile();
+                    }
+                }
+                else if (dr == DialogResult.No)
+                {
+                    openFile();
+                }
+            }
+            else
+            {
+                openFile();
+            }
+        }
+
+        private void openFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select file";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = openFileDialog.FileName;
+                string fileText = File.ReadAllText(file);
+                _txtbox.Text = fileText;
+            }
+        }
+
+
+        private void _txtbox_KeyDown(object sender, KeyEventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            DialogResult dr;
+
+            if (e.Control && e.KeyCode == Keys.R)
+            {
+                dr = colorDialog.ShowDialog();
+                if (dr == DialogResult.OK)
+                {
+                    _txtbox.ForeColor = colorDialog.Color;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            BinaryWriter bW;
+            bW = new BinaryWriter(new FileStream("C:\\Users\\Zer0\\Documents\\temps\\config.bin", FileMode.OpenOrCreate));
+            int colorin = _txtbox.ForeColor.ToArgb();
+            bW.Write(colorin);
+            bW.Close();
+
+            StreamWriter sW = new StreamWriter(new FileStream("C:\\Users\\Zer0\\Documents\\temps\\tempText.txt",FileMode.OpenOrCreate));
+            sW.WriteLine(_txtbox.Text);
+            sW.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader sR = new StreamReader(new FileStream("C:\\Users\\Zer0\\Documents\\temps\\tempText.txt", FileMode.Open));
+                _txtbox.Text = sR.ReadToEnd();
+                sR.Close();
+
+                BinaryReader bR;
+                bR = new BinaryReader(new FileStream("C:\\Users\\Zer0\\Documents\\temps\\config.bin", FileMode.Open));
+                if (bR != null)
+                {
+                    Color texto = Color.FromArgb(bR.ReadInt32());
+                    _txtbox.ForeColor = texto;
+                }
+                bR.Close();
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+
+            }
         }
     }
 }
