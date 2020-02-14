4aa6a239002c274886a499155d04e9c60b70c558
Cesar <white.h0und.f0x@gmail.com>
2020-02-14 12:27:50 +0100

    [053] :)

--- Desarrollo de Interfaces/Tema 4/Tema4_DI/DI_Ex_08/Form1.Designer.cs
+++ Desarrollo de Interfaces/Tema 4/Tema4_DI/DI_Ex_08/Form1.Designer.cs
@@ -30,19 +30,23 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
+            this._btnAbrir = new System.Windows.Forms.Button();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this._btnAnterior = new System.Windows.Forms.Button();
+            this._btnSiguiente = new System.Windows.Forms.Button();
+            this._lblDirectorio = new System.Windows.Forms.Label();
+            this._lblInfoImagen = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // _btnAbrir
             // 
-            this.button1.Location = new System.Drawing.Point(319, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Abrir";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this._btnAbrir.Location = new System.Drawing.Point(62, 12);
+            this._btnAbrir.Name = "_btnAbrir";
+            this._btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this._btnAbrir.TabIndex = 0;
+            this._btnAbrir.Text = "Abrir";
+            this._btnAbrir.UseVisualStyleBackColor = true;
+            this._btnAbrir.Click += new System.EventHandler(this.Button1_Click);
             // 
             // imageList1
             // 
@@ -50,23 +54,69 @@
             this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
             this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             // 
+            // _btnAnterior
+            // 
+            this._btnAnterior.Location = new System.Drawing.Point(12, 68);
+            this._btnAnterior.Name = "_btnAnterior";
+            this._btnAnterior.Size = new System.Drawing.Size(75, 23);
+            this._btnAnterior.TabIndex = 1;
+            this._btnAnterior.Text = "Anterior";
+            this._btnAnterior.UseVisualStyleBackColor = true;
+            this._btnAnterior.Click += new System.EventHandler(this._btnAnterior_Click);
+            // 
+            // _btnSiguiente
+            // 
+            this._btnSiguiente.Location = new System.Drawing.Point(108, 68);
+            this._btnSiguiente.Name = "_btnSiguiente";
+            this._btnSiguiente.Size = new System.Drawing.Size(75, 23);
+            this._btnSiguiente.TabIndex = 2;
+            this._btnSiguiente.Text = "Siguiente";
+            this._btnSiguiente.UseVisualStyleBackColor = true;
+            // 
+            // _lblDirectorio
+            // 
+            this._lblDirectorio.AutoSize = true;
+            this._lblDirectorio.Location = new System.Drawing.Point(148, 17);
+            this._lblDirectorio.Name = "_lblDirectorio";
+            this._lblDirectorio.Size = new System.Drawing.Size(85, 13);
+            this._lblDirectorio.TabIndex = 3;
+            this._lblDirectorio.Text = "Directorio Actual";
+            // 
+            // _lblInfoImagen
+            // 
+            this._lblInfoImagen.AutoSize = true;
+            this._lblInfoImagen.Location = new System.Drawing.Point(208, 68);
+            this._lblInfoImagen.Name = "_lblInfoImagen";
+            this._lblInfoImagen.Size = new System.Drawing.Size(63, 13);
+            this._lblInfoImagen.TabIndex = 4;
+            this._lblInfoImagen.Text = "Info Imagen";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(339, 163);
+            this.Controls.Add(this._lblInfoImagen);
+            this.Controls.Add(this._lblDirectorio);
+            this.Controls.Add(this._btnSiguiente);
+            this.Controls.Add(this._btnAnterior);
+            this.Controls.Add(this._btnAbrir);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Image Viewer";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button _btnAbrir;
         private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button _btnAnterior;
+        private System.Windows.Forms.Button _btnSiguiente;
+        private System.Windows.Forms.Label _lblDirectorio;
+        private System.Windows.Forms.Label _lblInfoImagen;
     }
 }
 
--- Desarrollo de Interfaces/Tema 4/Tema4_DI/DI_Ex_08/Form1.cs
+++ Desarrollo de Interfaces/Tema 4/Tema4_DI/DI_Ex_08/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace DI_Ex_08
 {
@@ -16,13 +17,55 @@ namespace DI_Ex_08
         {
             InitializeComponent();
         }
+        OpenFileDialog FD = new OpenFileDialog();
+        Form form2 = new Form();
+        FileInfo[] files;
+        string strFilePath;
+
+
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.ShowDialog();
-            Form form2 = new Form();
-            form2.BackgroundImage = openFileDialog.FileName;
+            try
+            {
+
+                if (FD.ShowDialog() == DialogResult.OK)
+                {
+                    string fileToOpen = FD.FileName;
+                    FileInfo File = new FileInfo(fileToOpen);
+                    form2.BackgroundImage = Image.FromFile(fileToOpen);
+                }
+
+
+                FileInfo fInfo = new FileInfo(FD.FileName);
+                strFilePath = fInfo.DirectoryName;
+                files = fInfo.Directory.GetFiles();
+                _lblDirectorio.Text = strFilePath;
+                _lblInfoImagen.Text = String.Format("Nombre: " + fInfo.Name + "\nExtesion: " + fInfo.Extension + "\nTemaño" + fInfo.Length);
+
+                form2.Size = new Size(form2.BackgroundImage.Width, form2.BackgroundImage.Height);
+                form2.FormBorderStyle = FormBorderStyle.FixedSingle;
+                form2.Show();
+            }
+            catch (System.ObjectDisposedException)
+            {
+                form2.Show();
+            }
+
         }
+
+
+
+        private void form2_Closing(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();
+            e.Cancel = true;
+        }
+
+        private void _btnAnterior_Click(object sender, EventArgs e)
+        {
+            form2.BackgroundImage = Image.FromFile(strFilePath + "\\" + files[2].ToString());
+        }
+
     }
 }
