a03f7db26031c1f6c2cde6d47cfa9e99db864066
Cesar <white.h0und.f0x@gmail.com>
2020-02-17 11:31:18 +0100

    [054 A]

--- Sistemas de Servicios/Tema 2/Serv_Tema_2/ServEx05/Form1.Designer.cs
+++ Sistemas de Servicios/Tema 2/Serv_Tema_2/ServEx05/Form1.Designer.cs
@@ -28,41 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this._txtboxPatron = new System.Windows.Forms.TextBox();
+            this._txtboxDirectory = new System.Windows.Forms.TextBox();
+            this._txtboxResult = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // _txtboxPatron
             // 
-            this.textBox1.Location = new System.Drawing.Point(207, 28);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            this._txtboxPatron.Location = new System.Drawing.Point(323, 63);
+            this._txtboxPatron.Name = "_txtboxPatron";
+            this._txtboxPatron.Size = new System.Drawing.Size(100, 20);
+            this._txtboxPatron.TabIndex = 0;
             // 
-            // textBox2
+            // _txtboxDirectory
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 28);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
+            this._txtboxDirectory.Location = new System.Drawing.Point(39, 63);
+            this._txtboxDirectory.Name = "_txtboxDirectory";
+            this._txtboxDirectory.Size = new System.Drawing.Size(100, 20);
+            this._txtboxDirectory.TabIndex = 1;
             // 
-            // textBox3
+            // _txtboxResult
             // 
-            this.textBox3.Location = new System.Drawing.Point(125, 205);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(313, 199);
-            this.textBox3.TabIndex = 2;
+            this._txtboxResult.Location = new System.Drawing.Point(50, 153);
+            this._txtboxResult.Multiline = true;
+            this._txtboxResult.Name = "_txtboxResult";
+            this._txtboxResult.Size = new System.Drawing.Size(313, 199);
+            this._txtboxResult.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(513, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // Form1
             // 
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this._txtboxResult);
+            this.Controls.Add(this._txtboxDirectory);
+            this.Controls.Add(this._txtboxPatron);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -72,9 +85,10 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox _txtboxPatron;
+        private System.Windows.Forms.TextBox _txtboxDirectory;
+        private System.Windows.Forms.TextBox _txtboxResult;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Sistemas de Servicios/Tema 2/Serv_Tema_2/ServEx05/Form1.cs
+++ Sistemas de Servicios/Tema 2/Serv_Tema_2/ServEx05/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,79 @@ namespace ServEx05
         public Form1()
         {
             InitializeComponent();
+        }
+        static string realPath = "";
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string actualPath = _txtboxDirectory.Text;
+            string[] words = actualPath.Split('\\');
+            string subStr;
+            string newWord;
+            realPath = "";
+            bool firstTime = true;
+            foreach (var word in words)
+            {
+                newWord = word;
+                if (word.StartsWith("%") && word.EndsWith("%"))
+                {
+                    subStr = word.Substring(1, word.Length - 2);
+                    newWord = Environment.GetEnvironmentVariable(subStr);
+                }
+                if (firstTime)
+                {
+                    realPath = realPath + newWord;
+                    firstTime = false;
+                }
+                else
+                {
+                    realPath = realPath + "\\" + newWord;
+                }
+
+
+
+
+            }
+
+            int contThread = 0;
+
+            string[] files = Directory.GetFiles(realPath);
+            Thread[] hilos = new Thread[files.Length];
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                hilos[i] = new Thread(wordSearch);
+                hilos[i].Start(files[i]);
+            }
+
+
+
+
+        }
+
+
+
+        delegate void Delega(string texto, TextBox t);
+
+        public void wordSearch(object files)
+        {
+            Delega d = new Delega(cambiaTexto);
+            int cont = 0;
+            if (files.ToString().EndsWith(".txt"))
+            {
+                string fileRead = File.ReadAllText(files.ToString());
+                if (fileRead.ToString().Contains(_txtboxPatron.Text))
+                {
+                    cont++;
+
+                    this.Invoke(d, _txtboxResult.Text + "" + cont, _txtboxResult);
+                }
+            }
+        }
+
+
+        private void cambiaTexto(string texto, TextBox t)
+        {
+            t.AppendText(texto + Environment.NewLine);
         }
     }
 }
