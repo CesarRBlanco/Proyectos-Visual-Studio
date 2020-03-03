6d4d13dd8d07e1c85be95a0a11d5188a6a292d3c
Cesar <white.h0und.f0x@gmail.com>
2020-03-03 11:12:12 +0100

    [069 A]

---
+++ Desarrollo de Interfaces/EXAMEN/Tema6_DI/Ex_DI_01/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Ex_DI_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.userControl11 = new Ex_DI_01.UserControl1();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // userControl11
+            // 
+            this.userControl11.Location = new System.Drawing.Point(57, 45);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Posicion = Ex_DI_01.UserControl1.ePosicion.IZQUIERDA;
+            this.userControl11.Separacion = 0;
+            this.userControl11.Size = new System.Drawing.Size(150, 20);
+            this.userControl11.TabIndex = 0;
+            this.userControl11.TextLbl = "UserControl1";
+            this.userControl11.TextTxt = "";
+            this.userControl11.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.UserControl11_KeyPress);
+            this.userControl11.KeyUp += new System.Windows.Forms.KeyEventHandler(this.UserControl11_KeyUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.userControl11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControl1 userControl11;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Desarrollo de Interfaces/EXAMEN/Tema6_DI/Ex_DI_01/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex_DI_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl11_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            this.Text = "Letra: " + e.KeyChar;
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (userControl11.Posicion == UserControl1.ePosicion.IZQUIERDA)
+            {
+                userControl11.Posicion = UserControl1.ePosicion.DERECHA;
+            }
+            else
+            {
+                userControl11.Posicion = UserControl1.ePosicion.IZQUIERDA;
+            }
+            this.Text = userControl11.Posicion.ToString();
+            
+        }
+
+        private void UserControl11_KeyUp(object sender, KeyEventArgs e)
+        {
+            this.Text = "hola";
+        }
+    }
+}
